410d32a826caa00199f765512d6cc561d14bd94b
dacmarcell <dacmarcell@gmail.com>
2024-12-12 22:32:45 -0300

    Refactor TemperatureAnalyzer to use async methods and integrate Prisma for data handling

--- temperature/TemperatureAnalyzer.ts
+++ temperature/TemperatureAnalyzer.ts
@@ -1,11 +1,81 @@
+import prisma from "../prisma/singleton";
+import { CONSOLE_COLORS, WARNINGS } from "../utils/constants";
 import { BaseAnalyzer } from "./BaseAnalyzer";
 
 export class TemperatureAnalyzer extends BaseAnalyzer {
   execute() {
     console.log("TemperatureAnalyzer execute");
-    this.analyze();
+    return this.analyze();
   }
-  analyze() {
-    console.log("TemperatureAnalyzer analyze");
+  async analyze() {
+    const baseAverage = await this.getBaseAverage();
+    const currentAverage = await this.getCurrentAverage();
+
+    if (currentAverage === baseAverage) {
+      console.log(CONSOLE_COLORS.GREEN, `[TemperatureAnalyzer] ${WARNINGS.OK}`);
+
+      return { state: WARNINGS.OK };
+    }
+
+    if (currentAverage > baseAverage) {
+      console.log(
+        CONSOLE_COLORS.YELLOW,
+        `[TemperatureAnalyzer] ${WARNINGS.PAY_ATTENTION}`
+      );
+
+      return { state: WARNINGS.PAY_ATTENTION };
+    }
+
+    if (currentAverage < baseAverage) {
+      console.log(
+        CONSOLE_COLORS.RED,
+        `[TemperatureAnalyzer] ${WARNINGS.CRITICAL}`
+      );
+
+      return { state: WARNINGS.CRITICAL };
+    }
+
+    return { state: null };
+  }
+
+  async addValue(value: number) {
+    await prisma.temperature.create({ data: { value } });
+  }
+
+  async getBaseAverage() {
+    const randomValues = await this.getFiveRandomValues();
+    return this.average(randomValues);
+  }
+
+  async getCurrentAverage() {
+    const values = await this.getFiveLatestValues();
+    return this.average(values);
+  }
+
+  private async getFiveRandomValues() {
+    const totalRecords = await prisma.temperature.count();
+    const skip = Math.floor(Math.random() * totalRecords);
+
+    const temperatureRandomValues = await prisma.temperature.findMany({
+      take: 5,
+      skip,
+      select: { value: true },
+    });
+
+    return temperatureRandomValues.map((temp) => temp.value);
+  }
+
+  private async getFiveLatestValues() {
+    const fiveLatestTemperatures = await prisma.temperature.findMany({
+      take: 5,
+      select: { value: true },
+      orderBy: { createdAt: "desc" },
+    });
+
+    return fiveLatestTemperatures.map((temp) => temp.value);
+  }
+
+  private average(list: number[]) {
+    return list.reduce((a, b) => a + b) / list.length;
   }
 }
